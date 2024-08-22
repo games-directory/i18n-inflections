18810ec9e4ada113c5f62295d8cff1737d1fcf67
pacMakaveli <pacMakaveli90@gmail.com>
2024-08-22 17:35:16 +0100

    chore: update the version according to semantic versioning, and not whilly nilly

--- lib/i18n_on_steroids/version.rb
+++ lib/i18n_on_steroids/version.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+# Reminder to self with how Semantic Versioning works:
+#
+# MAJOR version when you make incompatible API changes
+# MINOR version when you add functionality in a backward compatible manner
+# PATCH version when you make backward compatible bug fixes
+#
+# Additional labels for pre-release and build metadata are available as extensions to the MAJOR.MINOR.PATCH format.
+
 module I18nOnSteroids
-  VERSION = "0.1.1"
+  VERSION = "0.2.0"
 end
